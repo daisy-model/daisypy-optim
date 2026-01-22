582c8eeb34e4abc224f603130843ea230ae8c916
Silas <silas@di.ku.dk>
2026-01-22 10:29:18 +0100

    Mock problem wrapper for testing

---
+++ tests/mock_problem.py
@@ -0,0 +1,8 @@
+class MockProblem:
+    def __init__(self, parameters, objective_fn):
+        self.parameters = parameters
+        self.objective_fn = objective_fn
+
+    def __call__(self, parameter_values):
+        named_parameters = { p.name : value for p, value in zip(self.parameters, parameter_values) }
+        return self.objective_fn(**named_parameters)
