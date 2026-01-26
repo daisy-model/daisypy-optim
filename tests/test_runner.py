4dd8b7ea7063f1c45a9ba12670d97de77292b54d
Silas Ørting <silas.orting@pm.me>
2026-01-26 16:50:45 +0100

    Test runner

---
+++ tests/test_runner.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+from daisypy.optim import DaisyRunner
+
+EXPECTED = "Hello from Daisy"
+
+def test_runner(tmp_path):
+    # Assume we are on linux and daisy is installed
+    runner = DaisyRunner('daisy')
+    dai_path = Path(__file__).parent / 'hello.dai'
+    result = runner(str(dai_path), str(tmp_path))
+    assert result.returncode == 0
+    
+    daisy_log = tmp_path / 'daisy.log'
+    with daisy_log.open(encoding='utf-8') as f:
+        lines = [ line for line in f ]
+    assert len(lines) >= 2
+    assert lines[-2].strip() == EXPECTED
