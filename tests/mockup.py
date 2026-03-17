f6ae13e0ee7f1d4c1d5132d4db3092327adaf1e8
Silas <silas@di.ku.dk>
2026-03-17 12:42:05 +0100

    More test mockup

---
+++ tests/mockup.py
@@ -0,0 +1,40 @@
+# pylint: disable=too-few-public-methods
+from subprocess import CompletedProcess
+from daisypy.optim.file_generator import FileGenerator
+
+class MockFileGenerator(FileGenerator):
+    '''Mock file generator that always generates the paths it was constructed with'''
+    def __init__(self, paths):
+        self.paths = paths
+
+    def __call__(self, output_directory, params, tagged=True):
+        return self.paths
+
+
+class MockRunner:
+    '''Mock runner always returning a CompletedProcess with a specified returncode'''
+    def __init__(self, args=None, returncode=0):
+        self.args = args if args is not None else []
+        self.returncode = returncode
+
+    def __call__(self, dai_file, output_directory):
+        return CompletedProcess(self.args, self.returncode)
+
+class MockObjective:
+    '''Mock objective always returning a specific value'''
+    def __init__(self, value=0):
+        self.value = value
+
+    def __call__(self, daisy_output_directory):
+        return { 'mock' : self.value }
+
+
+class MockProblem:
+    '''Problem case for test purposes. Will evaluate an objective by forwarding parameters'''
+    def __init__(self, parameters, objective_fn):
+        self.parameters = parameters
+        self.objective_fn = objective_fn
+
+    def __call__(self, parameter_values):
+        named_parameters = { p.name : value for p, value in zip(self.parameters, parameter_values) }
+        return { 'mock' : self.objective_fn(**named_parameters) }
