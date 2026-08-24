c37b54a9e0d4093c3e1742105e3cb42940a906ff
Silas Nyboe Ørting <silas.orting@pm.me>
2026-08-24 11:14:35 +0200

    Replace MultiFileGenerator with dict in python file generation tests

--- tests/test_python_chemical_reaction.py
+++ tests/test_python_chemical_reaction.py
@@ -1,16 +1,16 @@
 from pathlib import Path
 from daisypy.optim import (
     CategoricalParameter,
-    DefaultLogger,
-    DaisySequentialOptimizer,
-    DlfDataExtractor,
-    PyFileGenerator,
     DaiFileGenerator,
-    MultiFileGenerator,
-    DaisyRunner,
     DaisyOptimizationProblem,
+    DaisyRunner,
+    DaisySequentialOptimizer,
+    DefaultLogger,
+    OutputSpec,
+    PyFileGenerator,
     ScalarObjective,
-    mse
+    Simulation,
+    mse,
 )
 from .markers import requires_daisy
 
@@ -18,10 +18,23 @@ from .markers import requires_daisy
 def test_python_chemical_reaction(tmp_path):
     '''Test that we can optimize parameters of a python function in a full sim setup'''
     data_dir = Path(__file__).parent / 'test-data' / 'python-chemical-reaction'
-    file_generator = MultiFileGenerator({
+    file_generators = {
         'py' : PyFileGenerator('daisy-react.py', template_file_path=data_dir / 'daisy-react.py'),
         'dai' : DaiFileGenerator('run.dai', template_file_path=data_dir / 'test-pyreact.dai'),
-    })
+    }
+
+    outputs = {
+        "field" : OutputSpec("soil_NO3_profile.dlf", "NO3")
+    }
+
+    simulations = {
+        "sim" : Simulation(file_generators, outputs)
+    }
+
+    outcome_specs = {
+        "NO3-outcome" : ("sim", "field", "NO3"),
+    }
+
 
     parameters = {
         'py' : [ CategoricalParameter('param', [0.24, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6]) ],
@@ -32,11 +45,16 @@ def test_python_chemical_reaction(tmp_path):
     runner = DaisyRunner('daisy')
 
     target_file = data_dir / 'target.csv'
-    data_extractor = DlfDataExtractor({"soil_NO3_profile.dlf" : "NO3"})
-    objective = ScalarObjective("NO3", data_extractor, target_file, "NO3", mse)
+    objective = ScalarObjective(
+        name="NO3",
+        target=target_file,
+        target_col="NO3",
+        outcome_name="NO3-outcome",
+        loss_fn=mse
+    )
 
     problem = DaisyOptimizationProblem(
-        runner, file_generator, objective, parameters, tmp_path, debug=True
+        runner, simulations, outcome_specs, {}, objective, parameters, tmp_path, debug=True
     )
     with DefaultLogger(tmp_path) as logger:
         optimizer = DaisySequentialOptimizer(problem, logger)
--- tests/test_run_with_python.py
+++ tests/test_run_with_python.py
@@ -1,7 +1,7 @@
 # pylint: disable=R0801
 from pathlib import Path
 from daisypy.optim import (
-    PyFileGenerator, DaiFileGenerator, MultiFileGenerator, DaisyRunner
+    PyFileGenerator, DaiFileGenerator, DaisyRunner
 )
 from .markers import requires_daisy
 
@@ -10,10 +10,10 @@ EXPECTED_ONE_FILE = "f(4) = 8, f(-1) = 6.75"
 def test_run_with_python(tmp_path):
     '''Test that we can run Daisy with generated dai and python files'''
     template_dir = Path(__file__).parent / 'templates'
-    generator = MultiFileGenerator({
+    generators = {
         'py' : PyFileGenerator('testing.py', template_file_path=template_dir / 'template.py'),
         'dai' : DaiFileGenerator('run.dai', template_file_path=template_dir / 'template.dai')
-    })
+    }
 
     params = {
         'py' : { 'a' : 0.25, 'b' : 7 },
@@ -22,7 +22,8 @@ def test_run_with_python(tmp_path):
 
     # Assume we are on linux and daisy is installed
     runner = DaisyRunner('daisy')
-    dai_file = generator(tmp_path, params)['dai']
+    files = { k : g(tmp_path, params[k], tagged=False) for k, g in generators.items() }
+    dai_file = files['dai']
     result = runner(dai_file, str(tmp_path))
     assert result.returncode == 0
 
@@ -44,11 +45,11 @@ def test_run_with_several_python_files(tmp_path):
     # 'util' generates a python script that is imported by 'py'. Filename and function name must
     # match what is defined in the 'py' file.
     # 'dai' generates the file run by Daisy. You should only generate one dai file.
-    generator = MultiFileGenerator({
+    generators = {
         'py' : PyFileGenerator('testing.py', template_file_path=template_dir / 'template2.py'),
         'util' : PyFileGenerator('util.py', template_file_path=python_dir / 'util.py'),
         'dai' : DaiFileGenerator('run.dai', template_file_path=template_dir / 'template.dai')
-    })
+    }
 
     params = {
         'py' : { 'a' : 0.25, 'b' : 7 },
@@ -58,7 +59,8 @@ def test_run_with_several_python_files(tmp_path):
 
     # Assume we are on linux and daisy is installed
     runner = DaisyRunner('daisy')
-    dai_file = generator(tmp_path, params)['dai']
+    files = { k : g(tmp_path, params[k], tagged=False) for k, g in generators.items() }
+    dai_file = files['dai']
     result = runner(dai_file, str(tmp_path))
     assert result.returncode == 0
 
