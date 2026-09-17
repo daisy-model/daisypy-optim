df4b543b941d35a8ae967652b9e895d5f11c378e
Silas Nyboe Ørting <silas.orting@pm.me>
2026-09-17 08:28:29 +0200

    Handle known flatpak startup error
    Flatpak can fail when many processes are started concurrently. We
    handle it by retrying the run after a small wait

--- daisypy/optim/runner.py
+++ daisypy/optim/runner.py
@@ -1,4 +1,5 @@
 import os
+import time
 import subprocess
 from pathlib import Path
 
@@ -6,7 +7,7 @@ class DaisyRunner:
     # pylint: disable=too-few-public-methods
     """Class that knows how to run daisy"""
 
-    def __init__(self, daisy_bin, daisy_home=None):
+    def __init__(self, daisy_bin, daisy_home=None, max_tries=3, backoff=0.01):
         """
         Parameters
         ----------
@@ -16,10 +17,22 @@ class DaisyRunner:
         daisy_home : str
           Path to daisy home directory containing lib/ and sample/
           If not None set DAISYHOME environment variable to daisy_home. Otherwise dont set DAISYHOME
+
+
         """
         self.daisy_bin = daisy_bin
         if daisy_home is not None:
             os.environ.update('DAISYHOME', daisy_home)
+        self.max_tries = max_tries
+        self.backoff = backoff
+        # The flatpak version of Daisy can fail due to a flatpak startup issue where the run is
+        # aborted when openat2(...) returns -1 EAGAIN (Resource temporarily unavailable)
+        # The problem is concurrency related, and can (often?/always?) be resolved by retrying.
+        # The emitted eror message from flatpak is
+        # error: Extension org.freedesktop.Platform.GL.default has invalid merge-dirs
+        self._retry_errors = set(
+            b'invalid merge-dirs'
+        )
 
     def __call__(self, dai_file, output_directory=None):
         """Run daisy
@@ -41,7 +54,21 @@ class DaisyRunner:
         args = [
             self.daisy_bin,
             "-q",
-            "-d", output_directory,
-            dai_file
+            "-d", str(output_directory),
+            str(dai_file)
         ]
-        return subprocess.run(args, check=False)
+        for i in range(max(1, self.max_tries)):
+            result = subprocess.run(args, capture_output=True, check=False)
+            if result.returncode == 0:
+                break
+            retry = False
+            for retry_error in self._retry_errors:
+                if result.stderr.find(retry_error) != -1:
+                    # This is a known error so retry after a short wait.
+                    print(f'Running "{dai_file}" failed ({i+1}/self.max_tries). Retrying')
+                    retry = True
+                    time.sleep(self.backoff)
+                    break
+            if not retry:
+                break
+        return result
