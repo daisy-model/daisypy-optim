03686245accaf1bdb2497de3d6c6a8c9b6a97b15
Silas Nyboe Ørting <silas.orting@pm.me>
2026-09-21 12:56:06 +0200

    Cross platform robust cpu count on python>=3.12

--- daisypy/optim/process_executor.py
+++ daisypy/optim/process_executor.py
@@ -12,5 +12,10 @@ class DaisyProcessExecutor(ProcessPoolExecutor):
         super().__init__(self.max_processes)
 
 def _process_cpu_count():
+    # pylint: disable=no-member
     # os.process_cpu_count is available from python 3.13
-    return len(os.sched_getaffinity(0))
+    if hasattr(os, 'process_cpu_count'):
+        return os.process_cpu_count()
+    if hasattr(os, 'sched_getaffinity'):
+        return len(os.sched_getaffinity(0))
+    return os.cpu_count()
